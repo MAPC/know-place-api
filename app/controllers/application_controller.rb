6e7170df7b01713cf24d3bc091fc64b790e2ebbb
Matt Cloyd <cloydster@gmail.com>
2015-09-23 14:47:31 -0400

    Handle unauthorized actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 require 'active_record/session_store'
 
 class ApplicationController < JSONAPI::ResourceController # ActionController::API
+  include Pundit
   include ActionController::HttpAuthentication::Token
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   prepend_before_action :authenticate_user_from_token
 
@@ -24,4 +27,12 @@ class ApplicationController < JSONAPI::ResourceController # ActionController::AP
     User.find_by(id: session[:user_id]) if session[:user_id]
   end
   helper_method :current_user
+
+  private
+
+    def user_not_authorized
+      render json: "You are not authorized to perform this action.",
+           status: :unauthorized
+    end
+
 end
