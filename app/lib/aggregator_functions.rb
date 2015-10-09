c3a8b2b68f7261a6e487751f01555014bc7495cd
Matt Cloyd <cloydster@gmail.com>
2015-10-09 14:52:54 -0400

    Add percent_and_moe functions

--- app/lib/aggregator_functions.rb
+++ app/lib/aggregator_functions.rb
@@ -9,24 +9,25 @@ module AggregatorFunctions
 
   def self.percent(numerators, denominators)
     value = _percent(numerators, denominators)
-    { value: value.round(R), margin: nil }
+    { value: value.round(R+2), margin: nil }
   end
 
   def self.sum_and_moe(values, margins)
-    # puts "\n\nv: #{values}, m: #{margins}\n\n"
     values, margins = MarginSweeper.new(values, margins).sweep
     value  = _sum values
     margin = Math.sqrt( _sum_of_squares(margins) )
     { value: value.round(R), margin: margin.round(R) }
   end
 
-  def self.percent_and_moe(universe, universe_margins, subset, subset_margins)
-    uni, uni_m = MarginSweeper.new(universe, universe_margins).sweep
-    sub, sub_m = MarginSweeper.new(subset,   subset_margins).sweep
+  def self.percent_and_moe(subset, subset_margins, universe, universe_margins)
+    subset = AggregatorFunctions.sum_and_moe(subset, subset_margins)
+    universe = AggregatorFunctions.sum_and_moe(universe, universe_margins)
+    a, b = subset[:value],  universe[:value]  # Values
+    c, d = subset[:margin], universe[:margin] # Margins
 
-    value  = _percent(uni, sub)
-    margin = _percent_moe(uni, uni_m, sub, sub_m)
-    { value: value.round(R), margin: margin.round(R) }
+    value  = _percent(a, b)
+    margin = _percent_moe(a, b, c, d)
+    { value: value.round(R+2), margin: margin.round(R+2) }
   end
 
   def self.median(values)
@@ -58,8 +59,20 @@ module AggregatorFunctions
        _sum(numerators) / _sum(denominators).to_f
     end
 
-    def self._percent_moe(universe, universe_margins, subset, subset_margins)
-      raise NotImplementedError
+    def self._percent_moe(a, b, c, d)
+      return (c/b) if a == b # Write a test case for if a == b (sub = universe)
+
+      under = under_the_root(a, b, c, d)
+      return (1/b) * Math.sqrt( _under_the_root(a, b, c, d) ) if under < 0
+      (1/b) * Math.sqrt( under )
+    end
+
+    def self.under_the_root(a, b, c, d)
+      c**2 - ( (a/b)**2 * d**2 )
+    end
+
+    def self._under_the_root(a, b, c, d)
+      c**2 + ( (a/b)**2 * d**2 )
     end
 
     # Credit where due:
--- test/lib/aggregator_functions_test.rb
+++ test/lib/aggregator_functions_test.rb
@@ -14,15 +14,36 @@ class AggregatorFunctionsTest < ActiveSupport::TestCase
     @zeros ||= [0,1,0,2]
   end
 
+  def bapl
+    @bapl ||= [0, 10, 0, 5]
+  end
+
+  def bapl_me
+    @bapl_me ||= [17, 5, 12, 2]
+  end
+
+  def adults
+    @adults ||= [100, 200, 300, 50]
+  end
+
+  def adults_me
+    @adults_me ||= [10, 12, 15, 5]
+  end
+
+
   test "sum" do
     expected = { value: 10, margin: nil }
     assert_equal expected, AggregatorFunctions.sum( array )
   end
 
   test "percent" do
-    expected = { value: 0.38, margin: nil }
+    expected = { value: 0.3846, margin: nil }
     assert_equal expected,
       AggregatorFunctions.percent( array, larger_array )
+
+    percent = { value: 0.0231, margin: nil }
+    assert_equal percent,
+      AggregatorFunctions.percent(bapl, adults)
   end
 
   test "median with even set" do
@@ -41,6 +62,14 @@ class AggregatorFunctionsTest < ActiveSupport::TestCase
     expected = { value: 26, margin: 5.48 }
     assert_equal expected,
       AggregatorFunctions.sum_and_moe( larger_array, array )
+
+    sum_subset = { value: 15, margin: 17.83 }
+    assert_equal sum_subset,
+      AggregatorFunctions.sum_and_moe(bapl, bapl_me)
+
+    sum_universe = { value: 650, margin: 22.23 }
+    assert_equal sum_universe,
+      AggregatorFunctions.sum_and_moe(adults, adults_me)
   end
 
   test "sum and margins with zero values" do
@@ -49,12 +78,39 @@ class AggregatorFunctionsTest < ActiveSupport::TestCase
       AggregatorFunctions.sum_and_moe( zeros, array )
   end
 
+
   test "percent and margins" do
-    expected = { value: 0.12, margin: 20.6 }
+    expected = { value: 0.0231, margin: 0.0274 }
     assert_equal expected,
-      AggregatorFunctions.percent_and_moe( zeros, array, array, larger_array )
+      AggregatorFunctions.percent_and_moe( bapl, bapl_me, adults, adults_me )
   end
 
+  test "percent and margins when universe == subset" do
+    expected = { value: 1.0, margin: 1.1887 }
+    assert_equal expected,
+      AggregatorFunctions.percent_and_moe( bapl, bapl_me, bapl, adults_me )
+  end
+
+  test "percent and margins when function under root is negative" do
+    # The root is negative in this example, testing the sign-swap case
+    # in AggregatorFunctions#_percent_moe
+    # under_the_root => -999998.0119
+    expected = { value: 1000, margin: 1000.0001 }
+    assert_equal expected,
+      AggregatorFunctions.percent_and_moe( [1000,0,0,0], [1,1,0,0], [1,0,0,0], [1,0,0,0] )
+  end
+
+  test "percent and margins when zero margin" do
+    assert_nothing_raised {
+      AggregatorFunctions.percent_and_moe( bapl, [0,0,0,0], bapl, adults_me )
+    }
+  end
+
+  test "percent and margins when zero universe" do
+    assert_nothing_raised {
+      AggregatorFunctions.percent_and_moe( bapl, bapl_me, [0,0,0,0], adults_me )
+    }
+  end
 
 
 end
