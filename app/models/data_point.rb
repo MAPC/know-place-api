ee5048a748e7ff9393027977f77d9ae193070c2c
Matt Cloyd <cloydster@gmail.com>
2015-10-16 13:51:14 -0400

    Validate data points for having columns in (which might conflict with those rake tasks somewhat), and update data points to have correct(er) data.

--- app/models/data_point.rb
+++ app/models/data_point.rb
@@ -6,9 +6,22 @@ class DataPoint < ActiveRecord::Base
   has_and_belongs_to_many :reports
 
   validates :aggregator_id, presence: true
+  validate :all_fields_exist
 
   def field_array
     @field_arr ||= fields.split(',').map(&:strip)
   end
+
+  private
+
+    def all_fields_exist
+      conn = GeographicDatabase.connection
+      messages = field_array.map do |field|
+        if conn.column_exists?( self.tables, field )
+          "field #{field} in table #{self.tables} does not exist"
+        end
+      end
+      messages.each { |message| errors.add(:fields, message) }
+    end
 
 end
